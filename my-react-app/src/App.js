1efc8be4e2d47cca48987d497a0c67cf496f1d7c
manichow2553-eng <manichow2553@gmail.com>
2025-09-05 09:13:23 +0000

    msg

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -5,7 +5,10 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Hi this is ghost</h1>
+        <label htmlFor='email'>Enter your email:</label>
+        <input className="email" id="email" type="email"/>
+        <h1>Hi this is Ghost</h1>
+         <>Welcome to the new website</>
       </header>
     </div>
   );
